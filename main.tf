5bbb6285d1245e0ddf5dae49d09c69498c6db472
Krenol <lauritzgroeger@gmail.com>
2021-01-24 16:27:52 +0100

    added azure backend

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azure = {
       source  = "azurerm"
-      version = "~> 2.2.0"
+      version = "~> 2.43.0"
     }
   }
 }
@@ -47,5 +47,30 @@ resource "azurerm_public_ip" "web_server_public_ip" {
   name                = "${var.resource_prefix}-public-ip"
   resource_group_name = azurerm_resource_group.webserver_server_rg.name
   location            = var.web_server_location
-  allocation_method   = "Dynamic"
+  allocation_method   = var.environment == "production" ? "Static" : "Dynamic"
+}
+
+resource "azurerm_network_security_group" "web_server_nsg" {
+  name                = "${var.resource_prefix}-nsg"
+  resource_group_name = azurerm_resource_group.webserver_server_rg.name
+  location            = var.web_server_location
+}
+
+resource "azurerm_network_security_rule" "web_server_nsg_rule_rdp" {
+  name                        = "${var.resource_prefix}-nsg-rule-rdp"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "TCP"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.webserver_server_rg.name
+  network_security_group_name = azurerm_network_security_group.web_server_nsg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "web_server_nsg_association" {
+  network_security_group_id = azurerm_network_security_group.web_server_nsg.id
+  network_interface_id      = azurerm_network_interface.web_server_nic.id
 }

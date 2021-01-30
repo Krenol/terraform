8d2d6c3362dfeb941c072537403349b32452003a
Krenol <lauritz_yannick.groeger@stud.tu-darmstadt.de>
2021-01-30 14:50:50 +0100

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install curl bash -y
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs npm
+DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs 
 npm install npm@latest -g
 npx create-react-app dummy
 cd dummy

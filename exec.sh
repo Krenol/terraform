ecab9adef63945fbc775125176ecd1b7851d9a36
Krenol <lauritz_yannick.groeger@stud.tu-darmstadt.de>
2021-01-30 12:33:39 +0100

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEBIAN_FRONTEND=noninteractive apt-get install nodejs npm -y
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install nodejs npm -y
 npm install npm@latest -g
 npx create-react-app dummy
 cd dummy
